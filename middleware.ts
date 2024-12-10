049ff89c3c3e938c2b2a22b2d67b3730aec4fe14
felipe-zuniga-tv <felipe.zuniga@transvip.cl>
2024-12-10 10:47:59 -0300

    middleware fix session

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 import { updateSession } from "./lib/auth";
+import { Routes } from "./utils/routes";
 
 export async function middleware(request: NextRequest) {
   try {
-    return await updateSession(request);
+    const response = await updateSession(request);
+    console.log(`Middleware: ${response}`)
+    
+    // Add redirect if no response
+    if (!response) {
+      return NextResponse.redirect(new URL(Routes.LOGIN, request.url));
+    }
+    
+    return response;
   } catch (error) {
     console.error('Middleware session update error:', error);
-    return NextResponse.next();
+    return NextResponse.redirect(new URL(Routes.LOGIN, request.url));
   }
 }
 
